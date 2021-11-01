ffb3242f6710300dbbe899002055b5280faf7b91
Joselen <JC233552@fi365.ort.edu.uy>
2021-11-01 18:54:15 -0300

    Alertar ante valores anormales

--- Obligatorio1/catalog/src/repositories/rankRepository.js
+++ Obligatorio1/catalog/src/repositories/rankRepository.js
@@ -2,27 +2,24 @@ const Repository = require('./repository');
 
 module.exports = class RankRepository {
     constructor() {
-        this.rankRepository = Repository.Rank;        
+        this.rankRepository = Repository.Rank;   
+        this.relations = ['propertiesObserved'];
     }
 
     async findAll() {
+        var query = { include: this.relations };
         let rank = await this.rankRepository.findAll(query);
         return rank;
     }
 
     async save(data) {
-        let rank = await this.rankRepository.create(data);
-        return rank;
-    }
-
-    async save(data) {
-        let rank = await this.rankRepository.create(data);
+        let rank = await this.rankRepository.create(data, { include: this.relations });
         return rank;
     }
 
     async findByProperty(property) {
         try {
-            let rank = await this.rankRepository.findOne({ Property: property});
+            let rank = await this.rankRepository.findOne({ Property: property,include: this.relations });
             return rank;
         } catch (err) {
             return null;
@@ -31,7 +28,7 @@ module.exports = class RankRepository {
 
     async findByName(name) {
         try {
-            let rank = await this.rankRepository.findOne({ Name: name});
+            let rank = await this.rankRepository.findOne({ Name: name, include: this.relations });
             return rank;
         } catch (err) {
             return null;
