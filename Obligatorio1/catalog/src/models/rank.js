0e91d59517e05412216b01be8d8ff8f74bfb8e47
Joselen <JC233552@fi365.ort.edu.uy>
2021-11-14 09:48:40 -0300

    Se agregan validaciones a las entidades sensor, propertyPBserved y rank

--- Obligatorio1/catalog/src/models/rank.js
+++ Obligatorio1/catalog/src/models/rank.js
@@ -7,9 +7,10 @@ const Rank = (schema, types) => {
             primaryKey: true,
             autoIncrement: true,
         },
-        initialValue: Sequelize.DECIMAL(10,5),
-        finalValue: Sequelize.DECIMAL(10,5)
+        initialValue: { type: Sequelize.DECIMAL(10, 5), allowNull: false },
+        finalValue: { type: Sequelize.DECIMAL(10, 5), allowNull: false }
     });
 };
+
 
 module.exports = Rank;
